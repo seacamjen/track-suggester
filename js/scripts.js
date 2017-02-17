167a5844f54c62369d4ac029417119d0241b5641
Cameron Jensen <jensenseanc@gmail.com>
2017-02-17 08:33:05 -0800

    Add form will fill in for name, radio for developing type, and drop down for Work environment preference. Add scripts functions to start defining the ids and classes from index. Add hide panel to css page.

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(function(){
+
+  $(".ownInfo").submit(function(){
+    var person1 = $("input#firstName").val();
+    var person2 =
+    $("input#lastName").val();
+
+    $(".firstname").text(person1);
+    $(".lastname").text(person2);
+
+    var side = $("input:radio[name=frontBack]:checked").val();
+
+    var work = $("#environment").val();
+
+    if (side === 'front'){
+      side =
+    }  else {
+      side =
+    }
+
+    $("#track").empty().append(side);
+    $(".panel").show();
+    event.preventDefault();
+  });
+});
