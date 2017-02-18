a2bc6b0033a7cad53faab989d2f1a0217507c080
Cameron Jensen <jensenseanc@gmail.com>
2017-02-17 16:17:19 -0800

    Add a paragraph to be shown before the results are posted.

--- js/scripts.js
+++ js/scripts.js
@@ -18,7 +18,7 @@ $(function(){
     if (startDate <= '2017-02-16') {
       alert ("This date is in the past, Please select a future Date")
       return false
-    } 
+    }
 
     var favoriteColor = $("#color").val();
 
@@ -30,24 +30,24 @@ $(function(){
       if (work === '1') {
         track = 'C#/.NET'
         $(".panelC, .resultDisplay").show();
-        $(".panelPHP, .panelCSS, .panelJava, .panelRuby").hide();
+        $(".panelPHP, .panelCSS, .panelJava, .panelRuby, .priorToResults").hide();
       } else if (work === '2') {
         track = 'Java/Android'
         $(".panelJava, .resultDisplay").show();
-        $(".panelPHP, .panelCSS, .panelRuby, .panelC").hide();
+        $(".panelPHP, .panelCSS, .panelRuby, .panelC, .priorToResults").hide();
       } else if (work === '3') {
         track = 'PHP/Drupal'
         $(".panelPHP, .resultDisplay").show();
-        $(".panelRuby, .panelCSS, .panelJava, .panelC").hide();
+        $(".panelRuby, .panelCSS, .panelJava, .panelC, .priorToResults").hide();
       } else {
         track = 'Ruby/Rails'
         $(".panelRuby, .resultDisplay").show();
-        $(".panelPHP, .panelCSS, .panelJava, .panelC").hide();
+        $(".panelPHP, .panelCSS, .panelJava, .panelC, .priorToResults").hide();
       }
     } else {
       track = 'CSS'
       $(".panelCSS, .resultDisplay").show();
-      $(".panelPHP, .panelRuby, .panelJava, .panelC").hide();
+      $(".panelPHP, .panelRuby, .panelJava, .panelC, .priorToResults").hide();
     }
     $("#track").empty().prepend(track);
   });
